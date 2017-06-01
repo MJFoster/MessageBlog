96d67b5169222a06f9eb962e57cded9e49122515
fosterm <fosterm@JRS01LT257>
2017-06-01 15:47:00 -0600

    Add project files.

---
+++ MessageBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MessageBlog.Startup))]
+namespace MessageBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
